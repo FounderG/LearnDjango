a9a3e116b2526be9997585fe908ed2f7bf6b1bc5
FounderG <founderinter@live.com>
2017-07-06 22:52:52 +0800

    Update 0706

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+from django.template.loader import get_template
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+	template = get_template('index.html')
+	posts = Post.objects.all()
+	now = datetime.now()
+	#post_lists = list()
+	html = template.render(locals())
+	#for count, post in enumerate(posts):
+		#post_lists.append("No.{}:".format(str(count))+str(post)+"<br>")
+	return HttpResponse(html)
